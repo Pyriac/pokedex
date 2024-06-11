0ac4ab861b2badf2996da45e58a7e5411901e24b
Pyriac <cyriacpineau@gmail.com>
2024-06-11 16:21:00 +0200

    using props for displaying cards

--- src/component/PokemonCard.jsx
+++ src/component/PokemonCard.jsx
@@ -1,27 +1,10 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  { name: "mew" },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[0];
-
+function PokemonCard({ pokemon }) {
   return (
-    <div>
-      <figure>
-        {pokemon.imgSrc !== "" ? (
-          <img src={pokemon.imgSrc} alt="" />
-        ) : (
-          <p>???</p>
-        )}
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="" /> : <p>???</p>}
 
-        <h2>{pokemon.name}</h2>
-      </figure>
-    </div>
+      <h2>{pokemon.name}</h2>
+    </figure>
   );
 }
 
