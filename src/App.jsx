2c01365b1c8d97a28cec21c3fb2e027a6e4de930
Pyriac <cyriacpineau@gmail.com>
2024-06-12 16:28:42 +0200

    adding state for pokemon index  and button below cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./component/PokemonCard";
 
@@ -7,14 +8,46 @@ const pokemonList = [
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
-  { name: "mew" },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
 ];
 
 function App() {
+  const [pokemonIndex, setIndex] = useState(0);
+
+  const HandleClickNegative = () => setIndex(pokemonIndex - 1);
+  const HandleClickPositive = () => setIndex(pokemonIndex + 1);
+
   return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
+    <>
+      <h2>Pokemon Index : {pokemonIndex + 1}</h2>
+
+      <div>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      </div>
+      {pokemonIndex > 0 ? (
+        <button onClick={HandleClickNegative}>Précedent</button>
+      ) : null}
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={HandleClickPositive}>Suivant</button>
+      ) : null}
+    </>
   );
 }
 
